1de4fb44fca4b0acedb1e584676be6d86ca61a74
EmersonSchula <emerson.schula@gmail.com>
2025-03-20 19:36:44 -0300

    remove ft_printf and get_next_line implementations; add new versions in separate directories

---
+++ rank3/gnl/get_next_line.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschula <<marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 14:44:27 by eschula           #+#    #+#             */
+/*   Updated: 2025/03/20 19:24:40 by eschula          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int    ft_strlen(char *str)
+{
+    int    i = 0;
+
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char    *ft_strdup(char *s)
+{
+    char    *dup;
+    int        len = 0;
+    int        i = 0;
+
+    len = ft_strlen(s) + 1;
+    dup = (char *)malloc(len * sizeof(char));
+    if (!dup)
+        return (NULL);
+    while (s[i])
+    {
+        dup[i] = s[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
+}
+
+char    *get_next_line(int fd)
+{
+    static char    buffer[BUFFER_SIZE];
+    static int    pos;
+    static int    size_read;
+    char        line[70000];
+    int            i = 0;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    while (1)
+    {
+        if (pos >= size_read)
+        {
+            size_read = read(fd, buffer, BUFFER_SIZE);
+            pos = 0;
+            if (size_read == 0)
+                break ;
+        }
+        line[i++] = buffer[pos++];
+        if (buffer[pos] == '\n')
+            break ;
+    }
+    line[i] = '\0';
+    if (i == 0)
+        return (0);
+    return (ft_strdup(line));
+}
+
+int    main(void)
+{
+    int    fd;
+
+    fd = open("test.txt", O_RDONLY);
+    printf("%s", get_next_line(fd));
+}
---
+++ rank3/gnl/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eschula <<marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 15:07:13 by eschula           #+#    #+#             */
+/*   Updated: 2025/03/20 15:09:46 by eschula          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#endif
